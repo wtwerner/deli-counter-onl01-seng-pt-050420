709c2a9decef646fbaca03f56f120e56783662c2
Tommy Werner <wtwerner@gmail.com>
2020-05-12 01:23:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.count == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "The next person in line is #{katz_deli[0]}."
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.pop
   end
 end
